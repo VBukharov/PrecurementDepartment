7a6f4c8389032a7687849770e59ce46a5d250ea7
Vladislav <vladislav.bukharov@gmail.com>
2016-10-20 17:39:00 +0300

    start project

---
+++ logic/src/main/java/org/bukharov/procurementDepartment/logic/App.java
@@ -0,0 +1,13 @@
+package org.bukharov.procurementDepartment.logic;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public void main()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ web/src/main/java/test/Test.java
@@ -0,0 +1,10 @@
+package test;
+
+import org.bukharov.procurementDepartment.logic.*;
+
+public class Test {
+  public static void main(String[] args) {
+    App app = new App();
+    app.main();
+  }
+}
